e5b877896a73993c7b72e32fc37e13da81957a34
sh.choi_MAC <csonghui339@gmail.com>
2022-07-18 12:49:26 +0900

    220718_4교시_로그인실패시 세팅

--- 05_MVC2/src/main/java/com/kh/mvc/member/controller/LoginServlet.java
+++ 05_MVC2/src/main/java/com/kh/mvc/member/controller/LoginServlet.java
@@ -34,11 +34,26 @@ public class LoginServlet extends HttpServlet {
 			session = request.getSession();
 			
 			session.setAttribute("loginMember", loginMember);
+			// 로그인이 완료되면 메인 페이지로 리다이렉트
+			response.sendRedirect(request.getContextPath() + "/");
+			
+		} else {
+			// 로그인 실패에 대한 에러 메세지를 띄어주고 메인 화면으로 이동
+			
+			/* 
+			 * 1) 공용으로 사용하는 에러 메세지 출력 페이지에 전달할 메세지와 메세지 출력 후 이동할 페이지를 request 객체에 저장한다.
+			*/
+			request.setAttribute("msg", "아이디 혹은 비밀번호가 일치하지 않습니다.");
+			request.setAttribute("location", "/");
+			
+			/*
+			 * 2) request 객체의 데이터를 유지해서 에러 메세지 출력 페이지에 전달하기 위해 forward() 실행
+			 *  - msg.jsp로 포워딩 시키기
+			 */
+			request.getRequestDispatcher("/views/common/msg.jsp").forward(request, response);
 			
 		}
 		
-		// 메인 페이지로 리다이렉트
-		response.sendRedirect(request.getContextPath() + "/");
 		
 		
 //		System.out.println(loginMember);
